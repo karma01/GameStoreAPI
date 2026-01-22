084a825930afc27b9e2842af50d0a0031f6f3ea9
karma <ps716573@gmail.com>
2026-01-22 20:29:56 +0545

    assembly changes

--- GameStore/obj/Debug/net10.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net10.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28aa92d3b6d0c078e1764be88935cc347f44828b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
